1cd00d0dd280cc9cc964095f2cb0b3f02da5e0e0
ayeraKhan <contactayera@gmail.com>
2025-03-20 03:51:39 +0500

    Configured Prisma with MongoDB and fixed deployment issues

--- app/api/medical-history/route.ts
+++ app/api/medical-history/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import  prisma  from "@/lib/prisma"; // If using Prisma
+import  prisma  from "@/lib/prisma"; 
 
 export async function POST(req: Request) {
   try {
